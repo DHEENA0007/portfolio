7d0ad0ee4380bafed6b565816d417794a5688c6d
dheena <dheena@kali.kali>
2025-09-30 16:35:02 +0530

    3

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio3/',
+  base: '/portfolio/',
   plugins: [react()],
   server: {
     allowedHosts: ['.ngrok-free.app', '.railway.app'],
